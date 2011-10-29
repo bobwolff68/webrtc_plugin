41bead6f0f10d2f4d0502cb94ff200ab936b6cf3
Robert Wolff <rwolff@xvdth.com>
2011-10-28 17:10:45 -0700

    Minor revision to support linux building under revision 839 of webrtc codebase.

--- prep_webrtc_linux.sh
+++ prep_webrtc_linux.sh
@@ -6,11 +6,11 @@ cd third_party/webrtc
 
 echo Getting webrtc from its repo into third_party/webrtc
 gclient config http://webrtc.googlecode.com/svn/trunk
-gclient sync -r 815 --force
+gclient sync -r 839 --force
 
 echo Resetting/Rebuilding project files...
 python trunk/build/gyp_chromium --depth=trunk ../../src/examples/cmdline_audio_peer/cmdline_audio_peer.gyp
 
 cd trunk
 make cmdline_audio_peer
-# output to be found in ./out/Debug/
+# output to be found in third_party/webrtc/trunk/out/Debug/
