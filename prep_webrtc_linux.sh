ec16aaa90ac45c34c4727085a5153ef8bcc38e0f
Robert Wolff <rwolff@xvdth.com>
2011-11-16 11:59:46 -0800

    Minor tweaks for a good build in Linux all the way through Plugin.

--- prep_webrtc_linux.sh
+++ prep_webrtc_linux.sh
@@ -7,16 +7,13 @@ then
 fi
 
 ./testdeps.sh
-if [ $?!=0 ]
+if [ $? != 0 ]
 then
   echo
   echo Required dependencies not present. Exiting.
   echo
   exit 1
 fi
-
-# Must copy ONE modification for Linux. (Hopefully this doesn't grow.)
-cp third_party_mods/webrtc/trunk/build/common.gypi third_party/webrtc/trunk/build
 
 # TODO - Need to check that gclient exists.
 # TODO - Make sure wget is present
@@ -28,6 +25,10 @@ gclient config http://webrtc.googlecode.com/svn/trunk
 gclient sync -r 839 --force
 
 cd ../..
+
+# Must copy ONE modification for Linux. (Hopefully this doesn't grow.)
+cp third_party_mods/webrtc/trunk/build/common.gypi third_party/webrtc/trunk/build
+
 ./rebuild_webrtc.sh clean
 ./rebuild_plugin.sh clean
 # output to be found in third_party/webrtc/trunk/out/Debug/
