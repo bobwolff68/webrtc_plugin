0a3cc0102b2ba66e3e92032e79e6caf7483560d2
Manjesh Malavalli <mmalavalli@xvdth.com>
2011-11-16 17:09:46 -0800

    Added DocMainPage.h for presenting an overview of the project in the main page (index.html) of the doxygen documentation.

---
+++ src/DocMainPage.h
@@ -0,0 +1,19 @@
+/**
+    \mainpage Project Overview.
+    The main purpose of this project is to make available to the web developer, a plugin-based set of
+    JavaScript APIs that facilitates embedding of peer-to-peer communication functionality in websites.
+    The plugin APIs utilize Google's implementation of the W3C's WebRTC specification in order to setup,
+    manage and teardown video/voice data paths between peers. The project, also as part of its initial
+    release, takes care of presence and signaling in the native code space, so that web developers
+    can concentrate on simple tasks like call, hangup, signin and signout. Currently, only voice
+    connections are supported, with an objective to add video communication facility in the near future. 
+
+    \section setup_p2p_conn Setting up a peer-to-peer connection.
+    There are two aspects to setting up a successful connection between peers. One is having a mechanism to 
+    exchange signaling messages between peers. This is achieved here through the peers signing in to a signin 
+    server, and forwarding any signaling messages to it, as it knows the location of the intended recipients.
+    All this is handled through TCP connections. The second aspect is the voice data connection between peers, 
+    which is handled through RTP/RTCP(UDP) connections, with STUN/TURN/ICE for NAT traversal.
+    GoCast::PeerConnectionClient handles the first aspect, while GoCast::PeerConnectionObserver, along with
+    webrtc::PeerConnectionClient handles the second.
+*/
